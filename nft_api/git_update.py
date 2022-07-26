6ed141d84507fd61f72bf23b5e722b4fa233954a
Abdulazeez Olabode <azeezolabode010@gmail.com>
2022-07-26 23:55:11 +0100

    Tweaks

--- nft_api/git_update.py
+++ nft_api/git_update.py
@@ -4,7 +4,7 @@ from . import app
 #Route for the GitHub webhook
 @app.route('/git-update', methods=['POST'])
 def git_update():
-    repo = git.Repo('./nfttraderio_api')
+    repo = git.Repo('./nfttraderapi')
     origin = repo.remotes.origin
     repo.create_head('master',
     origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
